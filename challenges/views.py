0a79d4c27ba6d8db8e40058989d12768c68c21fd
Luke Abatayo <47374947+LukeAbatayo@users.noreply.github.com>
2021-09-04 01:59:24 +0800

    added returning HTML

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,6 @@
 import challenges
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect 
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, response 
 from django.urls import reverse 
 
 monthly_challenges = {
@@ -35,6 +35,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
   try: 
     challenge_text = monthly_challenges[month]
-    return HttpResponse(challenge_text)
+    response_data = f"<h1>{challenge_text}</h1>"
+    return HttpResponse(response_data)
   except:
-    return HttpResponseNotFound("This month is not supported")
+    return HttpResponseNotFound("<h1>This month is not supported</h1>")
